1929b0cb91ec7354902252d83174644c130a9504
YKW93 <rhkddn1657@gmail.com>
2021-01-19 10:14:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_sancle
+
+A new Flutter application.
---
+++ android/app/src/main/kotlin/com/nexters/flutter_sancle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nexters.flutter_sancle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
