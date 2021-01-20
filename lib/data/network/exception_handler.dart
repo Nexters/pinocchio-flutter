e111e398396db1a9e0eb937c45a7df073dc661ff
YKW93 <rhkddn1657@gmail.com>
2021-01-20 22:00:27 +0900

    feat: 네트워크 에러 헨들링 클래스 추가

---
+++ lib/data/network/exception_handler.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sancle/presentation/app/bloc/auth_bloc.dart';
+import 'package:flutter_sancle/presentation/app/bloc/auth_event.dart';
+
+class ExceptionHandler {
+  static handleException(BuildContext context, DioError error,
+      {bool showsErrorMsg = true, String errorMsg = '잠시 후 다시 시도해주세요.'}) {
+    if (error.response.statusCode == 401) {
+      BlocProvider.of<AuthBloc>(context).add(LogoutRequested(context));
+    } else {
+      if (showsErrorMsg) {
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text(errorMsg)));
+      }
+    }
+  }
+}
