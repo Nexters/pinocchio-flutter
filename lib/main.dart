bb789777c1b7539ad38042dbbaf1be71e213dde9
YKW93 <rhkddn1657@gmail.com>
2021-01-22 21:35:45 +0900

    feat: 카카오톡 클라이언트 아이디 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sancle/presentation/app/app.dart';
+import 'package:kakao_flutter_sdk/all.dart';
 
 void main() {
+  KakaoContext.clientId = "57c1c9479dd978197a079c7df2db0905";
   runApp(MyApp());
 }
