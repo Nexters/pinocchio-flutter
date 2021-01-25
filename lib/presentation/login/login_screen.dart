58b71da3cc293b435c5f08f6a823bca8c7868400
YKW93 <rhkddn1657@gmail.com>
2021-01-25 22:35:06 +0900

    feat: 로그인 디자인 작업 완료

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -3,7 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sancle/presentation/login/bloc/login_event.dart';
 import 'package:flutter_sancle/presentation/main/main_screen.dart';
 import 'package:flutter_sancle/utils/constants.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:touchable_opacity/touchable_opacity.dart';
 import 'bloc/login_bloc.dart';
 import 'bloc/login_state.dart';
 
@@ -36,16 +37,122 @@ class _LoginScreenState extends State<LoginScreen> {
                 .showSnackBar(SnackBar(content: Text(DEFAULT_ERROR_MSG)));
           }
         },
-        child: Container(
-          child: Center(
-            child: RaisedButton(
-              child: Text('카카오 로그인'),
-              onPressed: () {
-                BlocProvider.of<LoginBloc>(context)
-                    .add(KakaoTalkLoginRequested());
-              },
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              "assets/images/login_background.png",
+              fit: BoxFit.cover,
             ),
-          ),
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 72.0, left: 30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset("assets/images/sancle_logo_white.png"),
+                  SizedBox(height: 22.0),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      '간편하게 로그인하고\n서비스를 이용해 보세요',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontFamily: 'nanum_square',
+                        color: sancleDarkColor,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                _buildKakaoLoginButton(),
+                SizedBox(height: 16.0),
+                _buildAppleLoginButton(),
+                SizedBox(height: 62.0),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildKakaoLoginButton() {
+    return TouchableOpacity(
+      activeOpacity: 0.6,
+      onTap: () {
+        BlocProvider.of<LoginBloc>(context).add(KakaoTalkLoginRequested());
+      },
+      child: Container(
+        width: double.infinity,
+        height: 52.0,
+        margin: EdgeInsets.symmetric(horizontal: 38.0),
+        decoration: BoxDecoration(
+          color: kakaoLoginColor,
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              alignment: Alignment.centerLeft,
+              child: SvgPicture.asset("assets/icons/ic_kakao_logo.svg"),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                '카카오톡으로 로그인',
+                style: TextStyle(
+                    color: sancleDarkColor,
+                    fontSize: 14.0,
+                    fontFamily: 'nanum_square',
+                    fontWeight: FontWeight.w700),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAppleLoginButton() {
+    return TouchableOpacity(
+      activeOpacity: 0.6,
+      onTap: () {
+        // TODO 애플 로그인 추후 작업
+      },
+      child: Container(
+        width: double.infinity,
+        height: 52.0,
+        margin: EdgeInsets.symmetric(horizontal: 38.0),
+        decoration: BoxDecoration(
+          color: blackColor,
+          borderRadius: BorderRadius.circular(50.0),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              alignment: Alignment.centerLeft,
+              child: SvgPicture.asset("assets/icons/ic_apple_logo.svg"),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: Text(
+                'APPLE로 로그인',
+                style: TextStyle(
+                    color: whiteColor,
+                    fontSize: 14.0,
+                    fontFamily: 'nanum_square',
+                    fontWeight: FontWeight.w700),
+              ),
+            )
+          ],
         ),
       ),
     );
