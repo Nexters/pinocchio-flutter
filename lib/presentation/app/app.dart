134584a938f4b30b5f215d5c83b47295865780e5
LDongGyu <ehdrbchd@naver.com>
2021-01-27 22:11:23 +0900

    feat: 세로모드 고정

--- lib/presentation/app/app.dart
+++ lib/presentation/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sancle/data/repository/auth_repository.dart';
 import 'package:flutter_sancle/presentation/splash/bloc/splash_bloc.dart';
@@ -10,6 +11,11 @@ import 'bloc/auth_bloc.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return RepositoryProvider<AuthRepository>(
       create: (context) {
         return AuthRepository();
