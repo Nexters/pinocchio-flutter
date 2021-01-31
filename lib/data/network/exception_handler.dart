0f8411415221c2405fcd3fb11c6d89f830fac0e5
YKW93 <rhkddn1657@gmail.com>
2021-01-31 21:55:56 +0900

    fix: DioError 내부에서 response값이 null 인 경우 대비

--- lib/data/network/exception_handler.dart
+++ lib/data/network/exception_handler.dart
@@ -8,7 +8,7 @@ import 'package:flutter_sancle/presentation/app/bloc/auth_event.dart';
 class ExceptionHandler {
   static handleException(BuildContext context, DioError error,
       {bool showsErrorMsg = true, String errorMsg = '잠시 후 다시 시도해주세요.'}) {
-    if (error.response.statusCode == 401) {
+    if (error.response?.statusCode == 401) {
       BlocProvider.of<AuthBloc>(context).add(LogoutRequested(context));
     } else {
       if (showsErrorMsg) {
