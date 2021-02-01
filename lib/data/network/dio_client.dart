51c3ebc177e1f291b74e6adf11973c60480b05f0
YKW93 <rhkddn1657@gmail.com>
2021-02-01 11:41:30 +0900

    feat: 토큰 갱신을 위한 interceptor 추가

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_sancle/data/model/token_response.dart';
+import 'package:flutter_sancle/data/prefs/user_token_manager.dart';
 
 const BASE_URL =
     "http://ec2-3-139-60-119.us-east-2.compute.amazonaws.com:8082/";
@@ -21,6 +23,62 @@ class DioClient {
   /// 헤더에 토큰을 담지 않는 API 사용 시 해당 메소드 사용 */
   Future<Dio> getDefaultClient() async {
     _dio.interceptors.clear();
+    return _dio;
+  }
+
+  /// 헤더에 토큰을 담는 API 사용 시 해당 메소드 사용 */
+  Future<Dio> getAuthApiClient() async {
+    _dio.interceptors.clear();
+    _dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (RequestOptions options) async {
+          TokenResponse token = await UserTokenManger.instance.getUserToken();
+          if (token == null) {
+            return options;
+          }
+          options.headers["Authorization"] = "Bearer " + token.authToken;
+          return options;
+        },
+        onResponse: (Response response) {
+          return response;
+        },
+        onError: (DioError error) async {
+          if (error.response?.statusCode == 401) {
+            RequestOptions options = error.response.request;
+
+            _dio.lock();
+            _dio.interceptors.requestLock.lock();
+            _dio.interceptors.responseLock.lock();
+            _dio.interceptors.errorLock.lock();
+
+            TokenResponse token = await UserTokenManger.instance.getUserToken();
+            if (token == null) {
+              return error;
+            }
+
+            return Dio().put(BASE_URL + "/auth/authToken", queryParameters: {
+              "refreshToken": token.refreshToken
+            }).then((response) async {
+              TokenResponse tokenResponse =
+                  TokenResponse.fromJson(response.data);
+              if (tokenResponse == null) {
+                return error;
+              }
+              await UserTokenManger.instance.setUserToken(tokenResponse);
+              options.headers["Authorization"] = "Bearer " + token.authToken;
+            }).whenComplete(() {
+              _dio.unlock();
+              _dio.interceptors.requestLock.unlock();
+              _dio.interceptors.responseLock.unlock();
+              _dio.interceptors.errorLock.unlock();
+            }).then((value) {
+              return _dio.request(options.path, options: options);
+            });
+          }
+          return error;
+        },
+      ),
+    );
     return _dio;
   }
 }
