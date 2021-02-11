d8e0c8498275cb3608d0944b56e29c2df1942488
YKW93 <rhkddn1657@gmail.com>
2021-02-11 15:34:38 +0900

    refactor: 토큰 response에 userId 값 추가

--- lib/data/model/token_response.dart
+++ lib/data/model/token_response.dart
@@ -1,11 +1,13 @@
 /// accessToken : "2fc85855-0480-472e-9d68-8f3cb8affd78"
 /// refreshToken : "4d214b30-469b-448c-8c92-4d37290e3333"
 /// expireDateTime : "2021-01-30T10:26:28.890Z"
+/// userId : "String"
 
 class TokenResponse {
   String _accessToken;
   String _expireDateTime;
   String _refreshToken;
+  String _userId;
 
   String get accessToken => _accessToken;
 
@@ -13,17 +15,24 @@ class TokenResponse {
 
   String get refreshToken => _refreshToken;
 
+  String get userId => _userId;
+
   TokenResponse(
-      {String accessToken, String expireDateTime, String refreshToken}) {
+      {String accessToken,
+      String expireDateTime,
+      String refreshToken,
+      String userId}) {
     _accessToken = accessToken;
     _expireDateTime = expireDateTime;
     _refreshToken = refreshToken;
+    _userId = userId;
   }
 
   TokenResponse.fromJson(dynamic json) {
     _accessToken = json["accessToken"];
     _expireDateTime = json["expireDateTime"];
     _refreshToken = json["refreshToken"];
+    _userId = json["userId"];
   }
 
   Map<String, dynamic> toJson() {
@@ -31,6 +40,7 @@ class TokenResponse {
     map["accessToken"] = _accessToken;
     map["expireDateTime"] = _expireDateTime;
     map["refreshToken"] = _refreshToken;
+    map["userId"] = _userId;
     return map;
   }
 }
