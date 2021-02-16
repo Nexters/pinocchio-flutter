efbaa9d480882fd7f6408fde453bb6bbef97bf55
LDongGyu <ehdrbchd@naver.com>
2021-02-16 20:43:53 +0900

    feat: category list 간격 및 height 조정

--- lib/presentation/category/category_screen.dart
+++ lib/presentation/category/category_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sancle/data/model/mypage_response.dart';
+import 'package:flutter_sancle/presentation/edit/edit_screen.dart';
 import 'package:flutter_sancle/utils/constants.dart';
 import 'package:flutter_sancle/utils/size_config.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
@@ -6,14 +8,20 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:touchable_opacity/touchable_opacity.dart';
 
 class CategoryScreen extends StatefulWidget {
+  final MyPageResponse data;
+  final String category;
+
   @override
   _CategoryScreenState createState() => _CategoryScreenState();
 
-  static Route route() {
+  const CategoryScreen({this.data, this.category}) : super();
+
+  static Route route(MyPageResponse event, String category) {
     return MaterialPageRoute(builder: (_) {
-      return CategoryScreen();
+      return CategoryScreen(data: event, category: category);
     });
   }
+
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
@@ -25,7 +33,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
         children: [
           Container(
             height: 56,
-            padding: EdgeInsets.symmetric(horizontal: 30),
+            padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(30)),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -39,23 +47,26 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     Navigator.pop(context);
                   },
                 ),
-                Text('title',
+                Text(widget.category,
                     style: TextStyle(
                         fontSize: getProportionateScreenHeight(16),
                         fontWeight: FontWeight.w700,
                         fontFamily: 'nanum_square')),
-                SizedBox(width: 10)
+                SizedBox(width: getProportionateScreenWidth(10))
               ],
             ),
           ),
-          SizedBox(height: 10),
-          ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(30)),
-              itemCount: 10,
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index) {
-                return clothItem(index);
-              })
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(30)),
+                itemCount: 10,
+                shrinkWrap: true,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (BuildContext context, int index) {
+                  return clothItem(index);
+                }),
+          )
         ],
       )),
     );
@@ -63,30 +74,55 @@ class _CategoryScreenState extends State<CategoryScreen> {
 
   Widget clothItem(int index) {
     return Container(
-      child: Slidable(
-        actionPane: SlidableDrawerActionPane(),
-        // actionExtentRatio: 0.25,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12.0),
-              boxShadow: [
-                BoxShadow(color: shadowColor, offset: Offset(1, 2), blurRadius: 2)
-              ]),
-          child: ListTile(
-            leading: SvgPicture.asset('assets/icons/red.svg'),
-            title: Text('자라에서 구매한 니트 최대 18자',
-                style: TextStyle(
-                    fontSize: getProportionateScreenHeight(14),
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'nanum_square')),
-          ),
-        ),
-        secondaryActions: <Widget>[
-          IconSlideAction(
-            color: Colors.blue,
-            iconWidget: SvgPicture.asset('assets/icons/delete.svg'),
-            onTap: () {},
+      child: Column(
+        children: [
+          SizedBox(height: getProportionateScreenHeight(10)),
+          Slidable(
+            actionPane: SlidableDrawerActionPane(),
+            // actionExtentRatio: 0.25,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                        color: shadowColor, offset: Offset(1, 2), blurRadius: 2)
+                  ]),
+              child: Padding(
+                padding: EdgeInsets.only(top: getProportionateScreenHeight(16), bottom: getProportionateScreenHeight(16), left: getProportionateScreenWidth(16)),
+                child: Row(
+                  children: [
+                    // SvgPicture.asset('assets/icons/red.svg'),
+                    Image.asset('assets/icons/red.png'),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('자라에서 구매한 니트 최대 18자',
+                        style: TextStyle(
+                            fontSize: getProportionateScreenHeight(14),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'nanum_square')),
+                    TouchableOpacity(
+                      activeOpacity: 0.6,
+                      child: Container(
+                          child: SvgPicture.asset("assets/icons/edit.svg")),
+                      onTap: () {
+                        Navigator.push(context, EditScreen.route());
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            secondaryActions: <Widget>[
+              Container(
+                child: IconSlideAction(
+                  color: Color.fromRGBO(241, 239, 238, 0),
+                  iconWidget: SvgPicture.asset('assets/icons/delete.svg'),
+                  onTap: () {},
+                ),
+              ),
+            ],
           ),
         ],
       ),
